4cecb079699df9e63d6f4b7394ebde15e13a0de6
Stefan Tolksdorf <40759013+sit79@users.noreply.github.com>
2019-07-10 13:34:36 +0200

    Feature/testing (#8)
    * testing active
    * typo corrected
    * attempt to fix prettier/eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    rules: {
-        indent: ["error", 2],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "never"],
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-        "no-console": 1
-      },
-      settings: {
-        react: {
-          version: require("./package.json").dependencies.react
-        }
-      }
-
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    // semi: ["error", "never"],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "no-console": 1
+  },
+  settings: {
+    react: {
+      version: require("./package.json").dependencies.react
+    }
+  }
 };
--- public/electron.js
+++ public/electron.js
@@ -1,22 +1,22 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow } = require("electron");
 
-const path = require("path")
-const isDev = require("electron-is-dev")
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 // require("./electron/menu.js")
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   if (isDev) {
     const {
       default: installExtension,
       REACT_DEVELOPER_TOOLS
-    } = require("electron-devtools-installer")
+    } = require("electron-devtools-installer");
     installExtension(REACT_DEVELOPER_TOOLS)
       .then(name => console.log(`Added Extension:  ${name}`))
-      .catch(err => console.log("An error occurred: ", err))
+      .catch(err => console.log("An error occurred: ", err));
   }
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -29,42 +29,42 @@ function createWindow() {
     // },
     minHeight: 550,
     minWidth: 650
-  })
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
-  )
+  );
 
   // Open the DevTools.
   if (isDev) {
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
   }
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", () => (mainWindow = null))
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on("activate", function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
