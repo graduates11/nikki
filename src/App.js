079e16b625e48e978cbfc2a87564595054005539
martynakrysinska <mlecharka@gmail.com>
2019-07-15 14:05:39 +0200

    initialized draft.js  editor component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-
+import TextEditor from "./components/TextEditor";
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return <TextEditor />;
   }
 }
 
