2b3769314e5c28a8e972b82fee3b13a60bad8663
martynakrysinska <mlecharka@gmail.com>
2019-07-16 15:38:00 +0200

    created basic grid system with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
 import SearchBar from "./components/SearchBar";
 import TextEditor from "./components/TextEditor";
 import EntriesByDate from "./components/entriesByDate";
+
+const styles = {
+  fullHeight: {
+    height: "100vh"
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <EntriesByDate />
-        <TextEditor />
+        <Container style={styles.fullHeight}>
+          <Row style={styles.fullHeight}>
+            <Col className="border border-dark">
+              <SearchBar />
+              <EntriesByDate />
+            </Col>
+            <Col className="border border-dark">
+              <TextEditor />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
