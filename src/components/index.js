3cb7ce545d23a24268fa52b18158bd2cb1cddf48
sit79 <stefan.tolksdorf@gmail.com>
2019-07-17 14:09:02 +0200

    another try to fix circleCI tests

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import EntriesByDate from "./EntriesByDate";
-import EntriesByTag from "./EntriesByTag";
-import DatePicker from "./DatePicker";
-import SearchBar from "./SearchBar";
-import TextEditor from "./TextEditor";
+import EntriesByDate from "../components/EntriesByDate";
+import EntriesByTag from "../components/EntriesByTag";
+import DatePicker from "../components/DatePicker";
+import SearchBar from "../components/SearchBar";
+import TextEditor from "../components/TextEditor";
 
 export { EntriesByDate, EntriesByTag, DatePicker, SearchBar, TextEditor };
