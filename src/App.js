61254dfa8a50b791cbc4ad1623b5f930a07be5af
Quoc-Khanh Truong <khanh.truong@hotmail.de>
2019-07-17 10:56:59 +0200

    Add the calendar, linked calendar to the entrylist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
-import SearchBar from "./components/SearchBar";
-import TextEditor from "./components/TextEditor";
-// import EntriesByDate from "./components/entriesByDate";
-import EntriesByTag from "./components/entriesByTag";
+//import SearchBar from "./components/SearchBar";
+//import TextEditor from "./components/TextEditor";
+import EntriesByDate from "./components/entriesByDate";
+//import EntriesByTag from "./components/entriesByTag";
+import MyCalendar from "./components/myCalendar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: ""
+    };
+  }
+
+  myDate = value => {
+    this.setState({
+      date: value
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <EntriesByTag />
-        <SearchBar />
-        <TextEditor />
+        <MyCalendar myDate={this.myDate} />
+        <EntriesByDate date={this.state.date} />
       </div>
     );
   }
---
+++ src/components/myCalendar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+import localization from "moment/locale/de";
+
+moment.locale("de", localization);
+
+class myCalendar extends Component {
+  state = {
+    date: new Date(),
+    convertedDate: null
+  };
+
+  componentDidMount() {
+    let date = moment(this.state.date).format("L");
+    this.setState({
+      convertedDate: date
+    });
+    this.props.myDate(date);
+  }
+
+  onChange = date => {
+    this.setState({ date });
+    const newdate = moment(date).format("L");
+    this.setState({ convertedDate: newdate });
+    this.props.myDate(newdate);
+  };
+
+  render() {
+    return (
+      <div>
+        <Calendar onChange={this.onChange} value={this.state.date} />
+      </div>
+    );
+  }
+}
+
+export default myCalendar;
