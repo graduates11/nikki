9b65b59394919c0cb63a866ca19c81bddaae5dbe
martynakrysinska <mlecharka@gmail.com>
2019-07-17 13:41:07 +0200

    created index.js file in components folder

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import EntriesByDate from "./entriesByDate";
+import EntriesByTag from "./entriesByTag";
+import myCalendar from "./myCalendar";
+import SearchBar from "./SearchBar";
+import TextEditor from "./TextEditor";
+
+export default {
+  EntriesByDate,
+  EntriesByTag,
+  myCalendar,
+  SearchBar,
+  TextEditor
+};
