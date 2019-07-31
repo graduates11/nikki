21b6817986e68f0892bb59c17c8db1ce50f43b00
sit79 <stefan.tolksdorf@gmail.com>
2019-07-31 11:45:22 +0200

    empty search result will display 'no entries found'

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -7,23 +7,27 @@ const SearchResult = () => {
     <div>
       {state.searchResult === undefined ? null : (
         <ul>
-          {state.searchResult.map(entry => (
-            <li
-              key={entry.id}
-              onClick={() => {
-                dispatch({
-                  type: "GET_SEARCH_ENTRY",
-                  payload: {
-                    date: state.date,
-                    entry
-                  }
-                });
-              }}
-            >
-              {entry.date}
-              {entry.title}
-            </li>
-          ))}
+          {state.searchResult.length === 0 ? (
+            <p>no entries found</p>
+          ) : (
+            state.searchResult.map(entry => (
+              <li
+                key={entry.id}
+                onClick={() => {
+                  dispatch({
+                    type: "GET_SEARCH_ENTRY",
+                    payload: {
+                      date: state.date,
+                      entry
+                    }
+                  });
+                }}
+              >
+                {entry.date}
+                {entry.title}
+              </li>
+            ))
+          )}
         </ul>
       )}
     </div>
