d4eb3b97a9fba710099d754c7e3c385b7360f493
Stefan Tolksdorf <40759013+sit79@users.noreply.github.com>
2019-08-09 14:51:23 +0200

    Enhancement/journal 168 (#177)
    * modal added for deleteEntry
    * modal for deleteEntry styled

--- src/components/DeleteEntry.js
+++ src/components/DeleteEntry.js
@@ -1,23 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Button, Modal, ModalBody, ModalHeader } from "reactstrap";
 import { Store } from "./Store";
 
 const DeleteEntry = props => {
   const { dispatch } = useContext(Store);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDeleteModal = e => {
+    e && e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <span
-      className="hover deleteButtonSpan"
-      style={{ margin: 0 }}
-      onClick={() => {
-        dispatch({
-          type: "DELETE_ENTRY",
-          payload: {
-            id: props.id
-          }
-        });
-      }}
-    >
-      <i className="far fa-times-circle fa-sm"></i>
-    </span>
+    <div>
+      <span
+        className="hover deleteButtonSpan"
+        style={{ margin: 0 }}
+        onClick={toggleDeleteModal}
+      >
+        <i className="far fa-times-circle fa-sm"></i>
+      </span>
+      <Modal isOpen={isOpen}>
+        <ModalHeader className="modalHeaderCentered">
+          Are you sure you want to delete this entry?
+        </ModalHeader>
+        <ModalBody className="openDatePicker">
+          <Button
+            color="white"
+            className="button button--antiman button--round-l button--text-medium"
+            onClick={toggleDeleteModal}
+          >
+            Cancel
+          </Button>{" "}
+          <Button
+            color="white"
+            className="button button--antiman button--round-l button--text-medium"
+            onClick={() => {
+              dispatch({
+                type: "DELETE_ENTRY",
+                payload: {
+                  id: props.id
+                }
+              });
+            }}
+          >
+            Delete
+          </Button>
+        </ModalBody>
+      </Modal>
+    </div>
   );
 };
 
